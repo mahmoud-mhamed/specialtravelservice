510cccb1abe90958f923e2216aca20d6e4fcdd81
mahmoud-mhamed <mahmoud1272022@gmail.com>
2025-05-30 02:31:42 +0300

    feat: Update app version to 1.9 and QNB credentials

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/version', function () {
-    return 1.8;
+    return 1.9;
 });
 Route::get('/', function () {
     return redirect()->route('dashboard.login.view-form');
